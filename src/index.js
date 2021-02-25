9a43dc22d460d0ec3f2410db41e7d19fbdaaa536
Eremor <artem.s13@gmail.com>
2021-02-25 16:56:47 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let i = 0;
+
+    while(i < bracketsConfig.length) {
+        let brk = bracketsConfig[i].join('')
+
+        if(str.includes(brk)) {
+            str = str.replace(brk, '')
+            i = 0;
+        } else {
+            i++
+        }
+    }
+
+ return str.length ? false : true
 }
